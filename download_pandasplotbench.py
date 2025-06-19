252e042276e539eef8c0265bb6c9c31280dcff24
Pedro Moreira <pedromoreirah3@gmail.com>
2025-06-18 23:01:51 -0400

    data organized

--- download_pandasplotbench.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-import os
-
-print("Downloading PandasPlotBench dataset...")
-dataset = load_dataset('JetBrains-Research/PandasPlotBench', split='test')
-dataset.to_json('data/pandasplotbench/tasks.jsonl')
-print(f'Downloaded {len(dataset)} tasks to data/pandasplotbench/tasks.jsonl') 
